667bfc5546be0c65ca3c330f589f9278cdcd3edd
Jason Raimondi <jason@raimondi.us>
2023-01-14 22:50:05 -0500

    chore: prettier format

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ module.exports = (opts = {}) => {
   // Work with options here
 
   return {
-    postcssPlugin: 'postcss-custom-media-generator',
+    postcssPlugin: "postcss-custom-media-generator",
     /*
     Root (root, postcss) {
       // Transform CSS AST here
@@ -25,7 +25,7 @@ module.exports = (opts = {}) => {
       }
     }
     */
-  }
-}
+  };
+};
 
-module.exports.postcss = true
+module.exports.postcss = true;
